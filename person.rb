e791a834720d5965818ddc9d6f5f2e64e3cd99d8
Busiwa24 <busiwaliuma@gmail.com>
2021-12-16 23:03:34 +0200

    Add working files

--- person.rb
+++ person.rb
@@ -1,36 +1,36 @@
-require_relative './corrector'
-
+# rubocop: disable Style/OptionalBooleanParameter
+require_relative 'corrector'
+require_relative 'rental'
 class Person
   attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1..1000)
-    @name = name
     @age = age
+    @name = name
     @parent_permission = parent_permission
     @corrector = Corrector.new
     @rentals = []
-  end
-
-  def add_rental(_rental)
-    Rental.new(date, self, book)
   end
 
   def of_age?
     @age >= 18
   end
 
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
   def validate_name
-    @name = @corrector.correct_name
+    @name = @corrector.correct_name(@name)
+  end
+
+  def add_rentals(date, book)
+    Rental.new(date, book, self)
   end
 
   private :of_age?
-
-  def can_use_services?
-    of_age? || parent_permission == true
-  end
 end
 
-me = Person.new(14, 'donard')
-p me.validate_name
+# rubocop: enable Style/OptionalBooleanParameter
